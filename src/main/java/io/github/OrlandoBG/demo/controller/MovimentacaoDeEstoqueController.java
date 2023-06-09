5f3b82df1b49da70bf1d894de8360a1b040941a9
OrlandoBG <orlandopsn1@gmail.com>
2023-06-09 15:45:05 -0300

    Implementação do teste obterPorItemDeEstoque de MovimentacaoDeEstoque

--- src/main/java/io/github/OrlandoBG/demo/controller/MovimentacaoDeEstoqueController.java
+++ src/main/java/io/github/OrlandoBG/demo/controller/MovimentacaoDeEstoqueController.java
@@ -3,9 +3,12 @@ package io.github.OrlandoBG.demo.controller;
 import io.github.OrlandoBG.demo.model.entities.MovimentacaoDeEstoque;
 import io.github.OrlandoBG.demo.model.services.MovimentacaoDeEstoqueService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -22,4 +25,15 @@ public class MovimentacaoDeEstoqueController {
         List<MovimentacaoDeEstoque> movimentacaoDeEstoqueList = service.ObterTodos();
         return ResponseEntity.ok().body(movimentacaoDeEstoqueList);
     }
+
+    @GetMapping(value = "/page")
+    public ResponseEntity<Page<MovimentacaoDeEstoque>> obterPorItemDeEstoque(@RequestParam(value="page", defaultValue="0") Integer page,
+                                                                             @RequestParam (value="size", defaultValue="1") Integer size,
+                                                                             @RequestParam (value ="itemDeEstoqueId", defaultValue="0")Long itemDeEstoqueId){
+        PageRequest pageRequest = PageRequest.of(page, size);
+        Page<MovimentacaoDeEstoque> movimentacaoDeEstoquepagina = service.ObterPorItemDeEstoque(pageRequest,itemDeEstoqueId);
+        return ResponseEntity.ok().body(movimentacaoDeEstoquepagina);
+    }
+
+
 }
--- src/main/java/io/github/OrlandoBG/demo/model/services/MovimentacaoDeEstoqueService.java
+++ src/main/java/io/github/OrlandoBG/demo/model/services/MovimentacaoDeEstoqueService.java
@@ -1,8 +1,12 @@
 package io.github.OrlandoBG.demo.model.services;
 
+import io.github.OrlandoBG.demo.model.entities.ItemDeEstoque;
 import io.github.OrlandoBG.demo.model.entities.MovimentacaoDeEstoque;
+import io.github.OrlandoBG.demo.model.repository.ItemDeEstoqueRepository;
 import io.github.OrlandoBG.demo.model.repository.MovimentacaoDeEstoqueRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,9 +18,19 @@ public class MovimentacaoDeEstoqueService {
     @Autowired
     private MovimentacaoDeEstoqueRepository repository;
 
+    @Autowired
+    private ItemDeEstoqueRepository itemDeEstoqueRepository;
+
     @Transactional(readOnly = true)
     public List<MovimentacaoDeEstoque> ObterTodos(){
         return repository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public Page<MovimentacaoDeEstoque> ObterPorItemDeEstoque(PageRequest pageRequest, Long itemDeEstoqueId) {
+        ItemDeEstoque itemDeEstoque = (itemDeEstoqueId == 0 ) ? null : itemDeEstoqueRepository.getOne(itemDeEstoqueId);
+        Page<MovimentacaoDeEstoque> movimentacaoDeEstoquePagina = repository.obterPorItemDeEstoque(pageRequest, itemDeEstoque);
+        return movimentacaoDeEstoquePagina;
     }
 
 }
